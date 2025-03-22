9626944a7c744717cc2633115b49bd3619333653
ilian <heatblazer@gmail.com>
2025-03-22 19:06:57 +0200

    wip/auotls for Lets encrypt manager

--- requests-go/go.mod
+++ requests-go/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/gin-contrib/sse v1.0.0 // indirect
+	github.com/gin-gonic/autotls v1.1.1 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -26,6 +27,7 @@ require (
 	golang.org/x/arch v0.15.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
--- requests-go/server/server.go
+++ requests-go/server/server.go
@@ -3,9 +3,12 @@ package server
 import (
 	"casino/rest-backend/player"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 // todo: temp for testing - remove later
@@ -17,9 +20,10 @@ var players = []player.Player{
 
 // end todo
 type Server struct {
-	Host   string
-	Port   uint16
-	router *gin.Engine
+	Host    string
+	Port    uint16
+	router  *gin.Engine
+	autocrt autocert.Manager //member for certificates with Let's encrypt
 }
 
 func (srv *Server) SetHostPort(s string, p uint16) {
@@ -58,3 +62,35 @@ func postPlayers(ctx *gin.Context) {
 	players = append(players, komardjia)
 	ctx.IndentedJSON(http.StatusCreated, komardjia)
 }
+
+// do not use yet, will nbe needed for certificates for Let's encrypt
+func (srv *Server) autoCertManagement() {
+	srv.autocrt = autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("example.com", "example1.com"), // etc - todo for later
+		Cache:      autocert.DirCache("/var/www/.cache"),
+	}
+	log.Fatal(autotls.RunWithManager(srv.router, &srv.autocrt))
+}
+
+//todo - add the context and run with golang context also handle sigint / sigterm
+// for now will start with cert manager
+/*
+  // Create context that listens for the interrupt signal from the OS.
+  ctx, stop := signal.NotifyContext(
+    context.Background(),
+    syscall.SIGINT,
+    syscall.SIGTERM,
+  )
+  defer stop()
+
+  r := gin.Default()
+
+  // Ping handler
+  r.GET("/ping", func(c *gin.Context) {
+    c.String(http.StatusOK, "pong")
+  })
+
+  log.Fatal(autotls.RunWithContext(ctx, r, "example1.com", "example2.com"))
+}
+*/
