69da3b57567c3aafc61f632b6edeb59993767f33
ilian <heatblazer@gmail.com>
2025-03-25 15:51:34 +0200

    feat/yaml config and app confing

--- requests-go/main.go
+++ requests-go/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"casino/rest-backend/config"
 	"casino/rest-backend/server"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Printf("--- server up ---\r\n")
-	s := server.Server{}
-	s.SetHostPort("localhost", 8080)
+	if len(os.Args) != 2 {
+		fmt.Println("Error useage : provide yaml config file")
+		os.Exit(-1)
+	}
+	var conf = config.AppConfig{}
+	if err := conf.LoadConfig(os.Args[1]); err != nil {
+		fmt.Println("Failed to load cofig file")
+		os.Exit(-2)
+	}
+	fmt.Println("--- server up ---")
+	s := server.Server{Config: &conf}
 	s.DoRun()
 
 }
