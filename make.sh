3a17275baeef71ce0814adb167c29631640338bc
heatblazer <heatblazer@gmail.com>
2025-03-26 03:02:49 +0200

    added make all to bin

---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "start make casino all"
+
+mkdir bin
+
+cd login-service
+go build -o login-service main.go
+mv login-service ../bin
+
+cd ..
+
+cd requests-go
+go build  -o request-service main.go
+mv request-service ../bin
+cp config.yaml ../bin
+
+echo "end make casino all"
