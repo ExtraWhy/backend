3e2714f1cf6b1ec76a2f9a7061272408bd033fd7
heatblazer <heatblazer@gmail.com>
2025-03-27 23:23:00 +0200

    refacotr of makefile (see mr for info)

--- make.sh
+++ make.sh
@@ -2,13 +2,28 @@
 
 echo "start make casino all"
 
-go env -w GOPROXY=direct
+output="bin"
+
+ls -l | grep bin > /dev/null
+result=$?
+if [[ $result == 0 ]]; then
+	echo "Deleting "$output" folder..."
+	rm -rf $output
+fi
+
+
+login_service_name="user-service"
+request_service_name="reuests-service"
 
 internal_libs=""
 internal_libs_db=""
 internal_libs_logger=""
 internal_libs_player=""
 internal_libs_config=""
+
+
+go env -w GOPROXY=direct
+
 
 if [[ -z $1 ]]; then 
 echo "Setting default master repo"
@@ -25,39 +40,40 @@ echo "Using a branch for all internlas: "$1
 	internal_libs_player="github.com/ExtraWhy/internal-libs/player@"$1
 	internal_libs_config="github.com/ExtraWhy/internal-libs/config@"$1
 fi
-
+echo "--------------------------------------------------------------------------------"
 echo $internal_libs
 echo $internal_libs_db
 echo $internal_libs_logger
 echo $internal_libs_player
 echo $internal_libs_config
+echo "--------------------------------------------------------------------------------"
 
+mkdir $output
 
-mkdir bin
-
+echo "Prepare login service"
 cd login-service
 go get $internal_libs
 go get $internal_libs_db
 go get $internal_libs_logger
 go get $internal_libs_player
 go get $internal_libs_config
-
-go build -o login-service main.go
-mv login-service ../bin
-cp .env ../bin
-
+go build -o $login_service_name main.go
+mv $login_service_name "../"$output
+cp .env "../"$output
+echo "finished"
 cd ..
 
+echo "Preparing requests service"
 cd requests-go
 go get $internal_libs
 go get $internal_libs_db
 go get $internal_libs_logger
 go get $internal_libs_player
 go get $internal_libs_config
+go build  -o $request_service_name main.go
+cp gen-players.sh "../"$output
+mv $request_service_name "../"$output
+cp config.yaml "../"$output
+echo "finished"
 
-go build  -o request-service main.go
-cp gen-players.sh ../bin
-mv request-service ../bin
-cp config.yaml ../bin
-
-echo "end make casino all"
+echo "end make casino all :-)"
