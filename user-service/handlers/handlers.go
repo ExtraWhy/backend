b7b7ff5f399bd63c1a421f62cfb0352e55d4b38d
Trayko Traykov <carnophager@gmail.com>
2025-03-29 20:18:33 +0200

    Login service/oauth2 google (#14)
    * feat/db connector and test (#1)
    
    * feat/db connector and test
    
    * feat/db refactory
    
    * wip/readme update for sql setup
    
    * feat/yaml config and app confing
    
    * added make all to bin
    
    * added makefile to build all and move to bin folder
    
    * removed bin
    
    * Module wip (#6)
    
    * wip
    
    * detached player/db/confgi
    
    * added gen-players.sh for dummy test data: see readme
    
    * updated rest api service readme
    
    * Main readme update
    
    * chore makefile & readme (#8)
    
    * branch support (#9)
    
    * feat: added google and facebook authorization support
    
    * fix: updated go.sum
    
    ---------
    
    Co-authored-by: ilian zapryanov <heatblazer@gmail.com>

---
+++ user-service/handlers/handlers.go
@@ -0,0 +1,146 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"slices"
+
+	"github.com/ExtraWhy/internal-libs/config"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/facebook"
+	"golang.org/x/oauth2/google"
+)
+
+type OAuthHandler struct {
+	Config              *config.UserService
+	GoogleOAuthConfig   *oauth2.Config
+	FacebookOAuthConfig *oauth2.Config
+	cookieExpiry        int
+}
+
+func (handler *OAuthHandler) Init() error {
+	handler.GoogleOAuthConfig = buildOAuthConfig(handler.Config.GoogleProvider, google.Endpoint)
+	handler.FacebookOAuthConfig = buildOAuthConfig(handler.Config.FacebookProvider, facebook.Endpoint)
+
+	handler.cookieExpiry = 3600
+
+	gin_engine := gin.Default()
+	gin_engine.SetTrustedProxies(nil)
+
+	gin_engine.GET("/auth/google/login", handler.GoogleLogin)
+	gin_engine.GET("/auth/google/callback", handler.GoogleCallback)
+
+	gin_engine.GET("/auth/facebook/login", handler.FacebookLogin)
+	gin_engine.GET("/auth/facebook/callback", handler.FacebookCallback)
+
+	gin_engine.Run(":8080")
+
+	return nil
+}
+
+func buildOAuthConfig(provider config.OAuthProviderConfig, endpoint oauth2.Endpoint) *oauth2.Config {
+	return &oauth2.Config{
+		ClientID:     provider.ClientID,
+		ClientSecret: provider.ClientSecret,
+		RedirectURL:  provider.RedirectUrl,
+		Scopes:       provider.Scopes,
+		Endpoint:     endpoint,
+	}
+}
+
+func (handler *OAuthHandler) GoogleLogin(c *gin.Context) {
+	handler.performLogin(c, handler.GoogleOAuthConfig)
+}
+
+func (handler *OAuthHandler) GoogleCallback(c *gin.Context) {
+	handler.handleOAuthCallback(c, handler.GoogleOAuthConfig, handler.Config.GoogleProvider.UserInfoUrl, func(userInfo map[string]any) (string, string) {
+		familyName, _ := userInfo["family_name"].(string)
+		givenName, _ := userInfo["given_name"].(string)
+		picture, _ := userInfo["picture"].(string)
+		return fmt.Sprintf("%s %s", familyName, givenName), picture
+	})
+}
+
+func (handler *OAuthHandler) FacebookLogin(c *gin.Context) {
+	handler.performLogin(c, handler.FacebookOAuthConfig)
+}
+
+func (handler *OAuthHandler) FacebookCallback(c *gin.Context) {
+	handler.handleOAuthCallback(c, handler.FacebookOAuthConfig, handler.Config.FacebookProvider.UserInfoUrl, func(userInfo map[string]any) (string, string) {
+		firstName, _ := userInfo["first_name"].(string)
+		lastName, _ := userInfo["last_name"].(string)
+		picture := ""
+		// Facebook returns the picture as a nested object.
+		if picObj, ok := userInfo["picture"].(map[string]any); ok {
+			if data, ok := picObj["data"].(map[string]any); ok {
+				picture, _ = data["url"].(string)
+			}
+		}
+		return fmt.Sprintf("%s %s", firstName, lastName), picture
+	})
+}
+
+func (handler *OAuthHandler) performLogin(c *gin.Context, oauthConfig *oauth2.Config) {
+	state := c.Query("callback-url")
+
+	url := oauthConfig.AuthCodeURL(state, oauth2.AccessTypeOffline)
+	c.Redirect(http.StatusTemporaryRedirect, url)
+}
+
+func (handler *OAuthHandler) isSafeRedirect(rawURL string) bool {
+	parsedURL, err := url.Parse(rawURL)
+	fmt.Println(parsedURL, parsedURL.Host)
+	if err != nil || parsedURL.Host == "" {
+		return false
+	}
+
+	origin := fmt.Sprintf("%s://%s", parsedURL.Scheme, parsedURL.Host)
+	fmt.Println(origin)
+	return slices.Contains(handler.Config.AllowedHosts, origin)
+}
+
+func (handler *OAuthHandler) handleOAuthCallback(c *gin.Context, oauthConfig *oauth2.Config, userInfoURL string, extractUserData func(map[string]any) (username, photo string)) {
+	code := c.Query("code")
+	fmt.Println(code)
+	if code == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No code in request"})
+		return
+	}
+
+	token, err := oauthConfig.Exchange(c, code)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Token exchange failed"})
+		return
+	}
+
+	client := oauthConfig.Client(c, token)
+	resp, err := client.Get(userInfoURL)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get user info"})
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	var userInfo map[string]any
+	json.Unmarshal(body, &userInfo)
+
+	callbackURL := c.Query("state")
+	if !handler.isSafeRedirect(callbackURL) {
+		fmt.Println("NOT A SAFE REDIRECT origin", callbackURL)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	username, photo := extractUserData(userInfo)
+
+	c.SetCookie("token", token.AccessToken, handler.cookieExpiry, "/", callbackURL, true, true)
+	c.SetCookie("photo", photo, handler.cookieExpiry, "/", callbackURL, true, false)
+	c.SetCookie("username", username, handler.cookieExpiry, "/", callbackURL, true, false)
+
+	c.Redirect(http.StatusTemporaryRedirect, callbackURL)
+}
